5bef6f85085253bc67b8fca207b86ea2cae8e100
Kaan Corum <kaancorum3@gmail.com>
2026-02-07 00:21:51 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        System.out.println("Hello World 211");
+        System.out.println("Hello World a211");
     }
 }
